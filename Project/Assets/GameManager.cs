c2e9a12dda3f28c56aa1a981195a28a91e587826
Lenux1 <joohyun1603@naver.com>
2020-11-26 01:29:01 +0900

    enenmy_netflix1

---
+++ Project/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy_netflix;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateNetflix();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void CreateNetflix()
+    {
+        Vector3 pos = new Vector3(0, 6, 0);
+        Instantiate(enemy_netflix, pos, Quaternion.identity);
+    }
+}
